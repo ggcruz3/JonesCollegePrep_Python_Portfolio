ec73d5292374c52907be84593798ca579e62b36a
ggcruz3 <grecia58953@gmail.com>
2026-05-27 08:36:11 -0500

    Add files via upload

---
+++ pok.py
@@ -0,0 +1,192 @@
+#Sarah And Grecia
+#Pokemon Evolution. Create a function named info() that prints the name of the pokemon, its current level, and (extra challenge) some ascii art of the current pokemon.
+
+#Init
+import random
+pokemon_name = "Weedle"
+pokemon_level = 1
+day = 1
+
+#Func
+def Weedle():
+    print((r"              ,`.\n"))
+    print((r"              L  \\\n"))
+    print((r"             ,    \\\n"))
+    print((r"            j      \\\n"))
+    print((r"            ,       \\\n"))
+    print((r"           j         `\n"))
+    print((r"           ,          .__\n"))
+    print((r"        ,-'Y          `  `-.\n"))
+    print((r"     .-'    `..___..-'      `-.\n"))
+    print((r"    /__           ,-.          \\\n"))
+    print((r"   /(__)         `   '          `.\n"))
+    print((r"  |               `\"'             L\n"))
+    print((r"  `.------._                      |\n"))
+    print((r",'          `                     |\n"))
+    print(("F             |                    |\n"))
+    print(("|             |                    |\n"))
+    print(("`._         ,'                     j\n"))
+    print((r"  `+------'                      /\n"))
+    print((r"    \\                           /                         |`._\n"))
+    print((r"     `.                       ,'                          |   \\\n"))
+    print((r"       `._                _,-'                            |    \\\n"))
+    print((r"          `-,.________,.-'   `.                           |     L\n"))
+    print((r"           /                   '                          |     |\n"))
+    print((r"          /             _,._   |                          ,`---,'\n"))
+    print((r"        ,'|            /    .  j                        .'      `.\n"))
+    print((r"        . L            '    | ,                      ,-'\"'`-..   |\n"))
+    print((r"         .,\\            `--' / `.               ___./       ,.' ,'\n"))
+    print((r"            \\              ,'    \\__         ,-'     \"-.    | |'\n"))
+    print((r"             `-._______,.-'  __   | `'-._.,- ._        _`   `\"Y\n"))
+    print((r"               |           .\"  \\  |     \\      `.    ,'  \\   ,'\n"))
+    print((r"               |           '    | ;      .       .   `._./.-'\n"))
+    print((r"               7.           `'\"' / `.--. |   _.. |      j\n"))
+    print((r"               `.__       `   _-'   |   |j  /   ||     .'\n"))
+    print((r"                   `-...,_..-'      `--'/   `._, ^----'\n"))
+    print((r"                        .\\            _'       ,'\n"))
+    print((r"              `         `._-.______,.'`.___,.-'mh\n"))
+    print(("\n"))
+def Kakuna():
+    print((r"          _,--'\"\"\"\"\"\"---.._\n"))
+    print((r"        ,'                 `._\n"))
+    print((r"      ,'                      `.\n"))
+    print((r"    ,'                          \\\n"))
+    print((r"   .                             \\\n"))
+    print((r" ,'.                  ,-`.        \\\n"))
+    print((r"/   \\               ,'    ,        \\\n"))
+    print(("|`.  |\\            ,`      |         |\n"))
+    print(("L  `.| |         .''     _,'        _'\n"))
+    print((r"\\    \"'        ,`'_..-''        _,'\n"))
+    print((r" `.            '\"\"          _,.' `.\n"))
+    print((r"   /._                 _..-\"       \\\n"))
+    print((r"  /   `.          _,.-'             \\\n"))
+    print((r" /      \\-.___.--'/                  \\\n"))
+    print((r"|      ,/.     .-^+.._               F\n"))
+    print((r" L..-''.' \\  .'   |   `'--.....___   .\n"))
+    print((r" /     /   `/     |               `\"-;\n"))
+    print((r"/     j    j      '                ,'\n"))
+    print((r"`.    |    |       L          _.-'Y\n"))
+    print((r" ,`._/     |        .    _,.-'     .\n"))
+    print((r" `.  '|    |         \\\"\"\"|         |\n"))
+    print((r"  |   |    |         |   |         |\n"))
+    print((r"  |   |    |        ,'   |         |\n"))
+    print((r"  |   L    +      ,'     |         |\n"))
+    print((r"  |    \\    L    ,\\      j         |\n"))
+    print((r"  L     \\   |   /  `.   /          j\n"))
+    print((r"   \\    j\\  |  /    `. /          .\n"))
+    print((r"    L  .  ` | /       \\          /\n"))
+    print((r"    +  |   `|/                  /\n"))
+    print((r"     \\ | _,..._         \\      /\n"))
+    print((r"      ./'      `-._      \\   ,'\n"))
+    print((r"       l           `.     ^_/\n"))
+    print((r"       +             `   /\n"))
+    print((r"        L-\"\"--.       .,'\n"))
+    print((r"        |      `.     ,\n"))
+    print((r"        .        \\  ,'\n"))
+    print((r"         `       _.'\n"))
+    print((r"          `....-' mh\n"))
+def Beedrill():
+    print((r"                    ,--\"\"+--.\n"))
+    print((r"                   /     j   /`.\n"))
+    print((r"                  |     /   |   `.\n"))
+    print((r"                  |   ,'    '     \\\n"))
+    print((r"                  j,-'     '`..    \\\n"))
+    print((r"                 +      _ /    `._/ \\\n"))
+    print((r"                 |     / '-.     |   .\n"))
+    print((r"                 |    /     |   /    |\n"))
+    print((r"                 |   /     j   j     |\n"))
+    print((r"                 |  j      |   |     |._\n"))
+    print((r"                 | .'     7    |     |  `.\n"))
+    print((r" ___      _.._   | j      |    +     '    `.\n"))
+    print((r"|.---=-.,'+-. `. |/       F     L  ,'    ,'`.\n"))
+    print((r"||,==--'|_' |  j  \\      /      |,'   ,`'    L\n"))
+    print((r"'Y'   | |  '/ ',.-.\\    j     ,,^  _,' \\     |\n"))
+    print(("`.||   |  `.'  '    `.   / _,-'   `'     L   F\n"))
+    print((r" ||   `     .  ,-.   `,--'              |   |\n"))
+    print((r" `'    `.  /_,' ,'     `--------------\"\"\"\"'Y\n"))
+    print((r"        _:\"'_.-'       /_>:-.__           /\n"))
+    print((r"     `-\".`\"'__,`-.,-._/      `.\"\"`------\"'\n"))
+    print((r"     `.| `\"'      | | _.--'\"\"'--\\\n"))
+    print((r"      || /        | '\"  ___,.._  \\\n"))
+    print((r"     _|||__      / /,.-'       `- .\n"))
+    print((r"   ,'   `. .    /,'/'  _.,-\"\"\"--._F\n"))
+    print((r"   7     | |  .',L'|_-'           |\n"))
+    print((r"   +     | | / / ',\"'  ,.-'\"\"'`-._|\n"))
+    print((r"    L    ' |. /  .-.`\"'           |\n"))
+    print((r"    |   j j   \\  `-.'\\           j\n"))
+    print((r"    +   | | \\  `.   ` `.  _.... ,\n"))
+    print((r"     L  | |  \\   .   `  \\\"     /\n"))
+    print((r"     | ,' |   L  ,'    \\ `    .\n"))
+    print((r"     | || |   '  |      L `   |\n"))
+    print((r"     `./|j     `. .     `. \\ j\n"))
+    print((r"      |  '       ` .     | '\\`\n"))
+    print((r"                  \\ '.   | \\\n"))
+    print((r"                   | |  /,-'\n"))
+    print((r"                   j l  \"\n"))
+    print((r"                 _/_,'\n"))
+    print((r"                ',' mh\n"))
+
+def evolve():
+    global pokemon_name, pokemon_level
+    if pokemon_level >= 10 and pokemon_name != "Beedrill":
+        pokemon_name = "Beedrill"
+        print("Congratulations Kakuna Evolved Into Beedrill!!")
+        Beedrill()
+
+    elif pokemon_level >= 7 and pokemon_name == "Weedle":
+        pokemon_name = "Kakuna"
+        print("Congratulations Weedle Evolved Into Kakuna ")
+        Kakuna()
+
+def train():
+    global pokemon_level
+    leaves = input("How many leaves does Weedle want to eat? ")
+    print("Weedle ate " + leaves + " leaves!")
+    pokemon_level += 1
+    evolve()
+def battle():
+    global pokemon_level
+    x = random.randint(1, 2)
+    if x == 1:
+        print("You lost the battle :( You just lost a level ")
+        pokemon_level -= 1
+    else:
+        print("You won the battle! You gained two more levels")
+        pokemon_level += 2
+    evolve()
+
+def info():
+    print("Name: ", pokemon_name)
+    print("Level: ", pokemon_level)
+    print("Day: ", day)
+
+def main():
+    global day
+    print("Welcome to Pokemon Game! You have been assgiened to take care of Weedle which will grow into Beedrill as you train them!")
+
+    while True:
+        print(f"Day {day} - What adventure are you choosing today? ")
+        print("""
+        1.Train
+        2.Battle
+        3.Info
+        4.Quit
+        """)
+        choice = input("Choose an activity (1-5):")
+        if choice == "1":
+            train()
+        elif choice == "2":
+            battle()
+        elif choice == "3":
+            info()
+        elif choice == "4":
+            print("Thank You For Coming, Be Back Soon Goodbye!")
+            break
+        else:
+            print("That is not one of the options, Please try again ")
+
+        day += 1
+
+#Main
+main()
+
